782498d06f70025e04216ac7b95c4e4f9082aefb
Gabriel Kerekeš <gabriel.kerekes.ml@hotmail.com>
2017-03-09 21:56:56 +0100

    done

---
+++ App3/App3/QuotesPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace App3
+{
+    public partial class QuotesPage
+    {
+        private readonly string[] quotes = { "yes", "no", "maybe", "never" };
+        private int currentQuotesIndex;
+
+        public QuotesPage()
+        {
+            InitializeComponent();
+
+            currentQuotesIndex = 0;
+
+            UpdateQuote();
+        }
+
+        private void UpdateQuote()
+        {
+            QuoteLabel.Text = quotes[currentQuotesIndex];
+        }
+
+        private void NextButton_OnClicked(object sender, EventArgs e)
+        {
+            currentQuotesIndex++;
+            if (currentQuotesIndex >= quotes.Length)
+                currentQuotesIndex = 0;
+
+            UpdateQuote();
+        }
+    }
+}
